14bac46531c7ac6aad69802c4cc22f84486db37e
Jack Ta <jack.th.ta@gmail.com>
2023-04-04 14:41:49 -0700

    build: scaffold Webpack config to transpile using Babel
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// Core Node modules
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,7 +10,59 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Source: https://stackoverflow.com/questions/72318969/how-to-export-the-configuration-of-webpack-config-js-using-pure-esm
 export default {
   mode: "production",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js)$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              // Transpile JavaScript to be compatible with
+              // the least qualified target environment; polyfilling
+              // missing API functionality with `core-js`.
+              [
+                "@babel/preset-env",
+                {
+                  targets: "> 0.25%, not dead",
+                  useBuiltIns: "usage",
+                  corejs: "3.30.0",
+                },
+              ],
+
+              // Transpile JSX to JavaScript
+              [
+                "@babel/preset-react",
+                {
+                  // Use the new transform so JSX/TSX files
+                  // don't have to manually import the React library.
+                  // Source: https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+                  runtime: "automatic",
+                },
+              ],
+
+              // Transpile TypeScript to JavaScript
+              "@babel/preset-typescript",
+            ],
+
+            // Source: https://webpack.js.org/loaders/babel-loader/#exclude-libraries-that-should-not-be-transpiled
+            exclude: [
+              // \\ for Windows, / for macOS and Linux
+              /node_modules[\\/]core-js/,
+              /node_modules[\\/]webpack[\\/]buildin/,
+            ],
+            // Source: https://stackoverflow.com/a/68352125
+            sourceType: "unambiguous",
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".jsx", ".ts", ".js"],
+  },
   output: {
     filename: "main.bundle.js",
     path: path.resolve(__dirname, "build"),
