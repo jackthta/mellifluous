824e8e7f2455e2e1b2b26895889e1fdc472b737d
Jack Ta <jack.th.ta@gmail.com>
2023-04-10 15:59:46 -0700

    build(npm): extract `stylelint` config out from `package.json` into `stylelint.config.cjs` file
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ config/stylelint.config.cjs
@@ -0,0 +1,46 @@
+// Source: https://github.com/stormwarning/stylelint-config-recess-order#advanced
+
+const propertyGroups = require("stylelint-config-recess-order/groups");
+
+// Note to self: if the property ordering or grouping
+// doesn't work well with you, create your own ordering
+// config.
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  // `stylelint-order` is needed for the
+  // `"order/*"` rules which drive property
+  // ordering.
+  plugins: ["stylelint-order"],
+  rules: {
+    // 🦶(1)
+    "declaration-empty-line-before": null,
+
+    "order/properties-order": propertyGroups.map((group) => ({
+      ...group,
+
+      emptyLineBefore: "always",
+      noEmptyLineBetween: true,
+    })),
+  },
+};
+
+// FOOT NOTES
+
+/** 🦶(1)
+ *
+ *  Issue:
+ *    It was recommended by `stylelint-config-recess-order` to not extend
+ *    `stylelint-config-standard` (@see https://github.com/stormwarning/stylelint-config-recess-order#advanced).
+ *    However, without that config, `stylelint` would not do any syntax
+ *    checking, only the ordering of CSS properties. When I extended
+ *    `stylelint-config-standard`, hoping to receive back that functionality,
+ *    the ordering functionality from `stylelint-config-recess-order` stopped
+ *    working.. or seemed to be overridden by something within the `stylelint-config-standard`.
+ *
+ *  Solution:
+ *     It turns out that `stylelint-config-standard`'s use of the
+ *     `"declaration-empty-line-before"` rule was overriding `stylelint-config-recess-order`'s
+ *     `emptyLineBefore`/`noEmptyLineBetween` properties (I'm not sure whether it affected
+ *      one, the other, or both, but I need both properties' functionalities to not be overridden).
+ *
+ */
