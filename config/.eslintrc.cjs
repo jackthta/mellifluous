9cb21c336005bc87705ab0e9fe9d43ffd6f3a19a
Jack Ta <jack.th.ta@gmail.com>
2023-04-11 14:45:05 -0700

    build(`eslint`): add `eslint-plugin-react` related config to `.eslintrc.cjs`
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- config/.eslintrc.cjs
+++ config/.eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
+
   plugins: ["@typescript-eslint"],
+
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    sourceType: "module",
+
     project: true,
     tsconfigRootDir: "../",
   },
+
   root: true,
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
