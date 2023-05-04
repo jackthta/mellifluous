6b18746ea1c7aef904047b655137c28c223be66a
Jack Ta <jack.th.ta@gmail.com>
2023-05-04 15:10:24 -0700

    feat: create `axios` instance for `MusicBrainz` API
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ src/utilities/axios.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const MusicBrainz = axios.create({
+  baseURL: "https://musicbrainz.org/ws/2",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
